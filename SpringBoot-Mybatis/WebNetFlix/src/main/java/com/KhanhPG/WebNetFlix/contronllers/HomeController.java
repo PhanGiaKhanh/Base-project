23aa28685010ffe910da4f0ef0fa6d705060fedc
unknown <phangiakhanh90@gmail.com>
2022-02-18 14:14:04 +0700

    up fix

--- SpringBoot-Mybatis/WebNetFlix/src/main/java/com/KhanhPG/WebNetFlix/contronllers/HomeController.java
+++ SpringBoot-Mybatis/WebNetFlix/src/main/java/com/KhanhPG/WebNetFlix/contronllers/HomeController.java
@@ -1,13 +1,27 @@
 package com.KhanhPG.WebNetFlix.contronllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.KhanhPG.WebNetFlix.mapper.UserMapper;
+import com.KhanhPG.WebNetFlix.model.User;
+import com.KhanhPG.WebNetFlix.model.UserExample;
+
 @Controller
 public class HomeController {
+	@Autowired
+	UserMapper userMapper;
+	
 	@GetMapping(value = {"", "/home"})
 	public ModelAndView home() {
-		return new ModelAndView("index");
+		ModelAndView modelAndView = new ModelAndView("index");
+		UserExample example = new UserExample();
+		List<User> users =userMapper.selectByExample(example);
+		System.err.println("Check kiểm tra " + users.size());
+		return modelAndView;
 	}
 }
