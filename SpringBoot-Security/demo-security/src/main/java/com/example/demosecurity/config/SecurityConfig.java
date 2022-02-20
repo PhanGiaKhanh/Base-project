8eecf877c19663de2b3f44a6662adbda039cca80
unknown <phangiakhanh90@gmail.com>
2022-02-20 20:53:37 +0700

    up change page login default security

--- SpringBoot-Security/demo-security/src/main/java/com/example/demosecurity/config/SecurityConfig.java
+++ SpringBoot-Security/demo-security/src/main/java/com/example/demosecurity/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.demosecurity.config;
 
+import javax.swing.text.html.FormSubmitEvent;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,10 +37,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
+		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
-		http.addFilter(new CustomAuthenticationFilter(authenticationManager()));
+		http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(customAuthenticationFilter);
 	}
 	
 	@Bean
--- SpringBoot-Security/demo-security/src/main/java/com/example/demosecurity/filter/CustomAuthenticationFilter.java
+++ SpringBoot-Security/demo-security/src/main/java/com/example/demosecurity/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.example.demosecurity.filter;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
 * @author: KhanhPG
@@ -59,7 +63,12 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 				.withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
 				.withIssuer(request.getRequestURI())
 				.sign(algorithm);
-		response.setHeader("access_token", accessToken);
-		response.setHeader("refresh_token", refreshToken);
+		//	response.setHeader("access_token", accessToken);
+		//	response.setHeader("refresh_token", refreshToken);
+		Map<String, String> tokens = new HashMap<>();
+		tokens.put("access_token", accessToken);
+		tokens.put("refresh_token", refreshToken);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		new ObjectMapper().writeValue(response.getOutputStream(), tokens);
 	}
 }
