6a476278fd00bcd62d16f58d400b37059c7d4c26
unknown <phangiakhanh90@gmail.com>
2022-03-05 14:38:52 +0700

    first project spring websocket chatbox

---
+++ Spring-Websocket-ChatBox/Spring-Websocket-Chatbox/src/main/java/com/khanhpg/springwebsocket/SpringWebsocketChatboxApplication.java
@@ -0,0 +1,13 @@
+package com.khanhpg.springwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketChatboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketChatboxApplication.class, args);
+	}
+
+}
---
+++ Spring-Websocket-ChatBox/Spring-Websocket-Chatbox/src/test/java/com/khanhpg/springwebsocket/SpringWebsocketChatboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanhpg.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketChatboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
