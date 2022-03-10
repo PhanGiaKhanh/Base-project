c655805fd07cccdab82af39362afd3b0147d97bc
unknown <phangiakhanh90@gmail.com>
2022-03-10 16:58:31 +0700

    up

--- ReviewJava/ReviewJava/src/main/java/com/KhanhPG/review/main/CheckNonStaticInStatic.java
+++ ReviewJava/ReviewJava/src/main/java/com/KhanhPG/review/main/CheckNonStaticInStatic.java
@@ -6,7 +6,7 @@ package com.KhanhPG.review.main;
  */
 public class CheckNonStaticInStatic {
 	static {
-		String a = "a";
+		String a = "số 1";
 		System.out.println(a);
 	}
 	
@@ -16,14 +16,15 @@ public class CheckNonStaticInStatic {
 	}
 	public static void main(String[] args) {
 		CheckNonStaticInStatic.hello();
+		System.out.println("số 2");
 	}
 
 	static {
-		String a = "a none static";
+		String a = "sô 3";
 		System.out.println(a);
 	}
 	// Bắt buộc phải có hàm main mới chạy chương trình, ko có > Error: Main method not found in class...
 	// Không thể đổi tên main và String[]
 	// Có thể đổi trên args
-	// static xếp theo thứ tự chạy	
+	// static chạy trướcs
 }
--- ReviewJava/ReviewJava/src/main/java/com/KhanhPG/review/string/StringStringBuilderStringBuffer.java
+++ ReviewJava/ReviewJava/src/main/java/com/KhanhPG/review/string/StringStringBuilderStringBuffer.java
@@ -8,30 +8,32 @@ package com.KhanhPG.review.string;
 public class StringStringBuilderStringBuffer {
 	
 	public static void main(String[] args) {
-		String a = "a";
-		String af = new String("a");
-		String aa = "a" + "";
+		String a = "ab";
+		String aa = new String("ab");
+		String aaa = "a" + "b";
+		String aaaa = "a".concat("b");
+				
 		
-		StringBuffer aBuffer = new StringBuffer("a");
-		StringBuilder aBuilder = new StringBuilder("a");
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(aa));
+		System.out.println(System.identityHashCode(aaa));
+		System.out.println(System.identityHashCode(aaaa));
+//		
+//		System.out.println(a == af);
+		System.out.println(a == aaa);
 		
-		System.out.println(a.hashCode());
-		System.out.println(aa.hashCode());
-		System.out.println(af.hashCode());
+//		StringBuffer aBuffer = new StringBuffer("a");
+//		StringBuilder aBuilder = new StringBuilder("a");z`
+//
+//		System.out.println(aBuffer.hashCode());
+//		System.out.println(aBuilder.hashCode());
+//		
+//		// Chuyển về toString
+//		System.out.println(aBuffer.toString().hashCode());
+//		System.out.println(aBuilder.toString().hashCode());
 		
-		System.out.println(a == af);
-		System.out.println(a == aa);
-		
-
-		System.out.println(aBuffer.hashCode());
-		System.out.println(aBuilder.hashCode());
-		
-		// Chuyển về toString
-		System.out.println(aBuffer.toString().hashCode());
-		System.out.println(aBuilder.toString().hashCode());
-		
-		//  =/new String() là trong vùng nhớ string pool
-		// 	new StringBuilder/ new StringBuffer trong vùng nhớ head ngoài string pool
+		//  =  vùng nhớ string pool 
+		// 	new String / new StringBuilder/ new StringBuffer trong vùng nhớ head ngoài string pool
 		//	khi chuyển giá trị về toString() thì sẽ gán biến đó cho vùng nhớ string pool
 	}
 }
